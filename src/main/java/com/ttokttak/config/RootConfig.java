d0ead2420b2989477ac3b1cdb4ede97034747207
seoyeong <pyqw_@naver.com>
2026-06-23 16:36:57 +0900

    chore: RootConfig 및 DB 연결 설정
    - RootConfig: DataSource, MyBatis, 트랜잭션 설정
    - application.properties.example 추가
    - .gitignore에 application.properties 추가

---
+++ src/main/java/com/ttokttak/config/RootConfig.java
@@ -0,0 +1,64 @@
+package com.ttokttak.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackages = "com.ttokttak.service")
+@MapperScan(basePackages = "com.ttokttak.dao")
+public class RootConfig {
+    @Value("${jdbc.driver}")
+    private String driver;
+
+    @Value("${jdbc.url}")
+    private String url;
+
+    @Value("${jdbc.username}")
+    private String username;
+
+    @Value("${jdbc.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setMaximumPoolSize(5);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public SqlSessionFactoryBean sqlSessionFactory(
+            DataSource dataSource,
+            ApplicationContext applicationContext
+    ) throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setMapperLocations(
+                applicationContext.getResources("classpath:/mapper/**/*Mapper.xml")
+        );
+        factoryBean.setTypeAliasesPackage("com.ttokttak.domain");
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
